cf86b809f3cf8d140247d9a713f116d315ac9d78
supergnaw <loren.supernaw@gmail.com>
2022-12-07 19:47:08 -0600

    Create day-template.py

---
+++ 2022/day-template.py
@@ -0,0 +1,19 @@
+import re
+
+sample = """
+sample_input_goes_here
+"""
+input = sample.strip()
+
+# Load input file
+input = open("inputs/day-00.txt", 'r').read().strip()
+
+print("########## start pt 1 ##########")
+print("---------- wrong answers ----------")
+print({})
+print("========== end pt 1 ==========\n\n")
+
+print("########## start pt 2 ##########")
+print("---------- wrong answers ----------")
+print({})
+print("========== end pt 2 ==========\n\n")
