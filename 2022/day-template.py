54c78d5a10bf6fa46221c8f4f5364c2be9f35942
supergnaw <loren.supernaw@gmail.com>
2022-12-11 18:30:59 -0600

    Update day-template.py
    updating with some changes that I also made in my local file so far

--- 2022/day-template.py
+++ 2022/day-template.py
@@ -1,19 +1,38 @@
 import re
+import json
+import math
+import random
+import operator
 
-sample = """
+# Load sample data
+sample_1 = """
 sample_input_goes_here
 """
-input = sample.strip()
+sample_2 = """
+sample_input_goes_here
+"""
+sinput_1 = sample_1.strip()
+sinput_2 = sample_2.strip()
 
 # Load input file
-input = open("inputs/day-00.txt", 'r').read().strip()
+input = open("inputs/day-01.txt", 'r').read().strip()
 
-print("########## start pt 1 ##########")
-print("---------- wrong answers ----------")
-print({})
-print("========== end pt 1 ==========\n\n")
+print(f"{' Start Pt I ':#^60}")
+print(f"{' Wrong Answers Only ':-^60}")
+wrong_answers_only = {
+    "too high":  []
+    , "too low": []
+    , "other":   []
+}
+print(json.dumps(wrong_answers_only, sort_keys=False, indent=4))
+print(f"{' End Pt I ':=^60}\n")
 
-print("########## start pt 2 ##########")
-print("---------- wrong answers ----------")
-print({})
-print("========== end pt 2 ==========\n\n")
+print(f"{' Start Pt II ':#^60}")
+print(f"{' Wrong Answers Only ':-^60}")
+wrong_answers_only = {
+    "too high":  []
+    , "too low": []
+    , "other":   []
+}
+print(json.dumps(wrong_answers_only, sort_keys=False, indent=4))
+print(f"{' End Pt II ':=^60}\n")
